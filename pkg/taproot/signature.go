ebda06178dd6b2dcfecf4a69db7ad10760888e94
mpetrun5 <matija.petrunic@gmail.com>
2024-09-02 16:55:09 +0200

    Fix signature counter panic

--- pkg/taproot/signature.go
+++ pkg/taproot/signature.go
@@ -86,13 +86,15 @@ const SignatureLen = 64
 // can verify the integrity of the signature.
 type Signature []byte
 
-// SignatureCounter is an atomic counter used to add some fault
+// signatureCounter is an atomic counter used to add some fault
 // resistance in case we don't use a source of randomness for Sign
-var SignatureCounter atomic.Uint64
+var signatureCounter atomic.Uint64
 
-// init initializes `SignatureCounter` to a random value
+// init initializes `signatureCounter` to a random value
 func init() {
-	binary.Read(rand.Reader, binary.BigEndian, &SignatureCounter)
+	var randSigCounter uint64
+	binary.Read(rand.Reader, binary.BigEndian, &randSigCounter)
+	signatureCounter.Store(randSigCounter)
 }
 
 // Sign uses a secret key to create a new signature.
@@ -133,7 +135,7 @@ func (sk SecretKey) Sign(rand io.Reader, m []byte) (Signature, error) {
 		}
 	} else {
 		// Need to use atomics, because of potential multi-threading
-		ctr := SignatureCounter.Add(1)
+		ctr := signatureCounter.Add(1)
 		binary.BigEndian.PutUint64(a, ctr)
 	}
 
